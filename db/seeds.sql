6a0455d9403187116ea88295af8d8e7dbc5fa4fc
HarmanBoparai <erharmanboparai@gmail.com>
2022-10-19 19:34:32 -0400

    Created schema and seeds for the employee database

---
+++ db/seeds.sql
@@ -0,0 +1,17 @@
+INSERT INTO department(departmentname)
+VALUES ("Management"),
+       ("Sales"),
+       ("Engineering"),
+       ("Finance"),
+       ("Legal"),
+
+INSERT INTO employee_role( title,salary, department_id)
+VALUES ("Product Manager",100000,1),
+       ("Sales Person",40000,2),
+       ("Software Engineer",80000,3),
+       ("Finance Manager",90000,4),
+       ("Lawyer",75000,2),
+
+       
+
+       
