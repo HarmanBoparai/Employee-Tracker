9b7784039270d449dc3488d120f2beb3a1bd6303
HarmanBoparai <erharmanboparai@gmail.com>
2022-10-26 22:37:59 -0400

    created update employee function

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 //Packages required for the application
 const inquirer = require('inquirer');
 
+
 // Import and require mysql2
 const mysql = require('mysql2');
 
@@ -24,27 +25,7 @@ const generatePrompt = () => {
         message:'What would you like to do?',
         choices:["View All Employees","Add Employee","Update Employee Role","View All Roles","Add Role","View All Departments","Add Department","Exit"]
     },
-    // //{
-    //     type: 'input',
-    //     name: 'first_name',
-    //     message: 'What is the first name of the employee?',
-    //   },
-    //   {
-    //     type: 'input',
-    //     name: 'last_name',
-    //     message: 'What is the last name of the employee?',
-    //   },
-    //   {
-    //     type: 'list',
-    //     name: 'employee_role',
-    //     message:'Please select employee role?',
-    //     choices:["Product Manager","Sales Person","Software Engineer","Finance Manager","Lawyer"]
-    // },
-    // {
-    //     type: 'input',
-    //     name: ' Manager_name',
-    //     message: 'What is the last name of the manager?',
-    //   },
+  
     ]
     ).then(function(answers){
       if(answers.options==="View All Employees"){
@@ -92,26 +73,88 @@ function viewalldepartments(){
 }
 
 function addemployee(){
-  db.query("select id,first_name,last_name,manager_id from employee",(err,res)=>{
-    let emp_id =res.map((employee)=>{
-      return employee.id
-    })
-
-    let manager_id = res.map((manager)=>{
-      return manager.manager_id
-    })
-
-    console.log(manager_id)
-
   db.query("select * from employee_role",(err,res)=>{
     let role_id =res.map((role)=> {
       return role.id
     })
 
+    console.log(role_id)
+    let addemployeeQuestions =[{
+      type: 'input',
+      name: 'first_name',
+      message: 'What is the first name of the employee?',
+    },
+    {
+      type: 'input',
+      name: 'last_name',
+      message: 'What is the last name of the employee?',
+    },
+    {
+      type: 'list',
+      name: 'employee_role',
+      message:'Please select employee role?',
+      choices:role_id,
+  },
+  {
+      type: 'input',
+      name: 'manager_id',
+      message: 'What is the id of the manager?',
+    },
+  
+]
 
+inquirer.prompt(addemployeeQuestions).then((answers)=>{
+console.log(answers)
+db.query(`INSERT INTO EMPLOYEE ( first_name,last_name, role_id,manager_id) VALUES ('${answers.first_name}','${answers.last_name}','${answers.employee_role}','${answers.manager_id}')`,(err,result)=>{
+  if(err) console.log(err)
+  console.log(result)
+})
+})
   })
 
-})}
+  }
+  function updateemployeerole(){
+    db.query("select * from employee_role",(err,res)=>{
+      let role_id =res.map((role)=> {
+        return role.id
+      })
+
+      db.query("select * from employee",(err,res)=>{
+        let employee_name=res.map((employee)=>{
+          return employee.first_name
+        })
+        let updateemployeeQuestions =[{
+          type: 'list',
+          name: 'emp_name',
+          message: 'Select the employee from below?',
+          choices:employee_name,
+        },
+        {
+          type: 'list',
+          name: 'role_id',
+          message: 'Select the role to assign to the employee?',
+          choices:role_id,
+        },
+    
+    ]
+    inquirer.prompt(updateemployeeQuestions).then((answers)=>{
+      db.query(`UPDATE employee SET role_id=${answers.role_id} WHERE first_name='${answers.emp_name}'`,(err,result)=>{
+        if(err) console.log(err);
+        console.log(result)
+      })
+    })
+
+      })
+  
+      
+      
+  
+ 
+    })
+  
+    }
+
+
 
 
 generatePrompt();
